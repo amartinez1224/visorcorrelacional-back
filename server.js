033a2f0dd7410ca7ee7049477b3ed2184a5749f9
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-09-11 12:15:29 -0400

    Simple express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Back is on.');
+});
+
+app.listen(port, () => console.log(`Back is listening on port ${port}.`));
