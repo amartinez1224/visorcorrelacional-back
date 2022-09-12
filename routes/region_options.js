5e0c078f29eff6c7b8a5dc6d84d9d38b2a2b392e
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-09-12 10:15:15 -0400

    Added get region by Divipola

--- routes/region_options.js
+++ routes/region_options.js
@@ -67,6 +67,43 @@ router.get('/regionesPDET', (req, res) => {
   res.send(regionesPDET.concat(totales));
 });
 
+function obtenerTipoPorDivipola(divipola) {
+  let div = divipola;
+  if (div.length === 2) {
+    div += '000';
+  }
+  if (div.length === 5) {
+    const divTotales = totales.map((total) => total.divipola);
+    if (divTotales.includes(div)) {
+      return { lista: totales, tipo: 'total', div };
+    }
+    if (div.substring(0, 2) === '02') {
+      return { lista: regionesPDET, tipo: 'regionPDET', div };
+    }
+    if (div.substring(div.length - 3) === '000') {
+      return { lista: departamentos, tipo: 'departamento', div };
+    }
+    return { lista: municipios, tipo: 'municipio', div };
+  }
+  return { lista: null, tipo: null, divipola };
+}
+
+router.get('/divipola/:divipola', (req, res) => {
+  const { divipola } = req.params;
+  const { lista, tipo, div } = obtenerTipoPorDivipola(divipola);
+  console.log(divipola, tipo);
+  if (lista === null) {
+    res.sendStatus(404);
+  } else {
+    const region = lista.find((item) => item.divipola === div);
+    if (region === undefined) {
+      res.sendStatus(404);
+    } else {
+      res.send({ region, tipo });
+    }
+  }
+});
+
 module.exports = {
   router, cargarRegiones,
 };
